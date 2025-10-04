362eb13c139471894222f3e8e462e94f302eb751
VANSH PRATAP SINGH <vanshpratapsingh1121@gmail.com>
2025-10-05 01:53:44 +0530

    Experiment 5.3

--- server.js
+++ server.js
@@ -1,22 +1,21 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-const studentRoutes = require('./routes/studentRoutes');
+const productRoutes = require('./routes/productRoutes');
 
 const app = express();
-app.set('view engine', 'ejs');
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 // MongoDB connection
-mongoose.connect('mongodb://127.0.0.1:27017/studentDB', {
+mongoose.connect('mongodb://127.0.0.1:27017/ecommerceDB', {
     useNewUrlParser: true,
     useUnifiedTopology: true
 }).then(() => console.log('MongoDB connected'))
   .catch(err => console.log(err));
 
 // Routes
-app.use('/', studentRoutes);
+app.use('/api', productRoutes);
 
-// Start server
+// Start Server
 const PORT = 3000;
 app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
